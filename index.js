470aca6bcc39e73c172c6c5f9570e2eaf5f96d55
RileyManda <rileymanda69@gmail.com>
2023-06-09 23:10:57 +0200

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import CountSolutions from './count-people/countPeople.js';
+import CountSolutions from './count-people/countpeople.js';
 
 const cardContainer = document.querySelector('.counting-people-solution');
 
